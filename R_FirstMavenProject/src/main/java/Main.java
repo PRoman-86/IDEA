ad2004a32f032492709b4143433015fb4e808dcc
PRoman-86 <pkomputer17@gmail.com>
2021-04-24 00:26:31 +0300

    02 commit

---
+++ R_FirstMavenProject/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        complexAbbreviateTest();
+    }
+
+    public static void complexAbbreviateTest() {
+
+        System.out.println("Привет ...".equals(StringUtils.abbreviate("Привет всем, кто живет на Луне", 10))); // true
+        System.out.println("Привет всем, кто ...".equals(StringUtils.abbreviate("Привет всем, кто живет на Луне", 20))); // true
+        System.out.println(StringUtils.abbreviate(null, 2)); // null
+        System.out.println("".equals(StringUtils.abbreviate("", 1))); // true
+        System.out.println("12...".equals(StringUtils.abbreviate("123 456", 5))); //true
+
+    }
+
+}
